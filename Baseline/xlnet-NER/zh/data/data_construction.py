17ba06f5bfb86631dcb83f31c7231a0cda16561a
jojo <70991866+fcihraeipnusnacwh@users.noreply.github.com>
2021-01-15 10:43:32 +0800

    Update data_construction.py

--- Baseline/xlnet-NER/zh/data/data_construction.py
+++ Baseline/xlnet-NER/zh/data/data_construction.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 13 09:38:14 2021
 
-@author: yuansiyu
-"""
 dataset = []
 with open('test_result.txt', 'r', encoding="utf-8") as f:
     for data in f.readlines():
